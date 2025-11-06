09e50c0240b95a14c575020d14f304e567294144
yozzwangyy-afk <yozzwangyy@gmail.com>
2025-11-06 09:52:18 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import Projects from '../components/Projects'
 import About from '../components/About'
 import Contact from '../components/Contact'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Projects />
       <About />
       <Contact />
+      <Footer />
     </main>
   )
 }
