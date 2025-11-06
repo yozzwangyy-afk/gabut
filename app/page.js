8355a32e05aa7d14040cc772ddd6d054b9a59caf
yozzwangyy-afk <yozzwangyy@gmail.com>
2025-11-06 09:23:38 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,19 +1,17 @@
 import Header from '../components/Header'
 import Hero from '../components/Hero'
-import About from '../components/About'
 import Projects from '../components/Projects'
+import About from '../components/About'
 import Contact from '../components/Contact'
-import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
-      <About />
       <Projects />
+      <About />
       <Contact />
-      <Footer />
     </main>
   )
 }
