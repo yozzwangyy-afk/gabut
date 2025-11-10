3f1d7225fc58ad25cea6627da1bdf93eab409ba2
yozzwangyy-afk <yozzwangyy@gmail.com>
2025-11-10 08:28:10 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,31 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['i.ytimg.com', 'img.youtube.com'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY'
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin'
-          }
-        ],
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
